c4c3b36173f8389672406efe4a701918a545b30d
Soufiane DAHIMI <soufianedahimi01@gmail.com>
2021-05-18 13:44:49 +0100

    Add more react components , start communication between frontend and backend

--- realtimeGraph/graphApp/consumers.py
+++ realtimeGraph/graphApp/consumers.py
@@ -1,83 +1,92 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
-import json 
+import json
 from random import randint
 from asyncio import sleep
 import time
 import threading
 import serial
+
+
+class SensorData():
+    def __init__(self):
+        self.temperature = 0
+        self.humidity = 0
+
+    def __str__(self):
+        return json.dumps({'temperature': self.temperature, 'humidity': self.humidity})
+
+
+class ProcessThread(threading.Thread):
+
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.daemon = True
+        self.ArduinoSerial = ""
+        self.isConnected = False
+
+    def sendToArduino(self, data):
+        if self.isConnected:
+            self.ArduinoSerial.write(data.encode())
+            response = self.ArduinoSerial.readline().decode("ascii")
+            return response
+        return "nothing"
+
+    def run(self):
+        try:
+            self.ArduinoSerial = serial.Serial('COM9', '115200', timeout=20)
+            time.sleep(2)
+            print(self.ArduinoSerial.readline().decode("ascii"))
+            self.isConnected = True
+        except serial.SerialException:
+            print('you are not lucky !!')
+        for i in range(1000):
+            # print(self.sendToArduino("s"))
+            print('Random generated number', GraphConsumer.sensorData)
+            GraphConsumer.sensorData.temperature = randint(30, 40)
+            GraphConsumer.sensorData.humidity = randint(35, 55)
+            GraphConsumer.numberSec = randint(0, 100)
+            time.sleep(0.5)
+        GraphConsumer.onProcess = False
+
+
 class GraphConsumer(AsyncWebsocketConsumer):
-    onProcess = False 
+    onProcess = False
     numberSec = 0
+    sensorData = SensorData()
+    processThread = ProcessThread()
+
     def __init__(self):
         super().__init__()
-        if GraphConsumer.onProcess == False :
+        if GraphConsumer.onProcess == False:
             GraphConsumer.startProcess()
-            GraphConsumer.onProcess =True
-        else :
+            GraphConsumer.onProcess = True
+        else:
             print('Process already started')
+
     async def connect(self):
         await self.accept()
         print("connected")
-        for i in range(1000):
-            
-            await self.send(json.dumps({'value' :GraphConsumer.numberSec }))
-            await sleep(1)
+
     async def disconnect(self, close_code):
         print("Disconnected")
-        
+
     async def receive(self, text_data):
         """
         Receive message from WebSocket.
         Get the event and send the appropriate event
         """
+        print("data received")
         response = json.loads(text_data)
         event = response.get("event", None)
         message = response.get("message", None)
-        if event == 'MOVE':
-            # Send message to room group
-            await self.channel_layer.group_send(self.room_group_name, {
-                'type': 'send_message',
-                'message': message,
-                "event": "MOVE"
-            })
+        print(event, message)
 
-        if event == 'START':
-            # Send message to room group
-            await self.channel_layer.group_send(self.room_group_name, {
-                'type': 'send_message',
-                'message': message,
-                'event': "START"
-            })
+        if event == 'SENSOR':
+            await self.send(str(GraphConsumer.sensorData))
+        if event == 'ACTION':
+            print('exeecution of ',
+                  message['alpha'], ' and ', message['vitesse'])
 
-        if event == 'END':
-            # Send message to room group
-            await self.channel_layer.group_send(self.room_group_name, {
-                'type': 'send_message',
-                'message': message,
-                'event': "END"
-            })
     @staticmethod
     def startProcess():
-        ProcessThread().start()
-        
-
-class ProcessThread(threading.Thread):
-
-    def __init__(self ):
-            threading.Thread.__init__(self)
-            self.daemon = True
-    def run(self):
-
-        print('call')
-        try :
-            self.ArduinoSerial = serial.Serial('COM16', '115200', timeout=20)
-            time.sleep(2)
-            print(self.ArduinoSerial.readline().decode("ascii"))
-            
-        except serial.SerialException :
-            print('you are not lucky !!')
-        for i in range(100):
-            print('Random generated number' , GraphConsumer.numberSec )
-            GraphConsumer.numberSec = randint(0,20)
-            time.sleep(1)
-        GraphConsumer.onProcess = False
+        GraphConsumer.processThread.start()
